ab0245531d019f1825e220eeb4ac3ff970bf43b5
Abdulaziz57 <a.mannai@hotmail.com>
2025-01-20 13:38:58 +0300

    created the refrences for each of the buttons and label

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var elapsedTimeLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func startButtonTapped(sender: UIButton) {
+        // code to start the timer
+    }
+    
+    @IBAction func stopButtonTapped(sender: UIButton) {
+        // code to stop the timer
     }
 
 
